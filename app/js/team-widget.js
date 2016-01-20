e35408f2e9c72b673a84a240efef417c570130be
Daniel Beeke <daniel@studiofonkel.nl>
2016-01-20 14:07:18 +0100

    up

--- app/js/team-widget.js
+++
@@ -1,4 +0,0 @@
-// $('.team-widget .employee').on('click', function() {
-// 	$('.team-widget .employee.toggleInfo').not(this).removeClass('toggleInfo');
-//     $(this).toggleClass('toggleInfo');
-// })
