d0c0d4eccea7da386d2e44b15a84035668e8a376
Theo Bouwman <theo@studiofonkel.nl>
2016-01-29 11:14:46 +0100

    commit

--- app/js/contact.js
+++ app/js/contact.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
             {
                 from: 'contact@studiofonkel.nl',
                 subject: 'Bericht via studiofonkel.nl',
-                to: ['theobouwman98@gmail.com'],
+                to: ['info@studiofonkel.nl'],
                 fromNameField: 'name',
                 phoneNumber: 'phone',
                 replyToField: 'email'
