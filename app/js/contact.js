1cb3d5b9733b2ddaff60c92f3a215a472aa95812
Ewout <ewout@studiofonkel.nl>
2016-08-10 09:16:36 +0200

    up

--- app/js/contact.js
+++ app/js/contact.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
         extraLine: true // Adds an extra line at the end of the textarea. Try both and see what works best for you.
     });
 
-
-    $('#contact-form, #vacancy-form').emailMandrill({
+    $('#contact-form').emailMandrill({
         mandrillKey: 'kg4HpxglJL9JPtqhAh6O5g',
         wait: function () {
             $('body').addClass('mandrill-waiting');
@@ -22,6 +21,35 @@ $(document).ready(function() {
             {
                 from: 'website@studiofonkel.nl',
                 subject: 'Bericht via studiofonkel.nl',
+                to: ['info@studiofonkel.nl'],
+                fromNameField: 'name',
+                phoneNumber: 'phone',
+                replyToField: 'email'
+                // fromName: 'John Smith',
+                // toField: '',
+                // replyTo: 'mail@domain.com',
+            }
+        ]
+    });
+
+    $('#vacancy-form').emailMandrill({
+        mandrillKey: 'kg4HpxglJL9JPtqhAh6O5g',
+        wait: function () {
+            $('body').addClass('mandrill-waiting');
+            $('#contact-form button').text('Verzenden');
+        },
+        success: function () {
+            $('body').removeClass('mandrill-waiting');
+            $('body').addClass('mandrill-success');
+            $('#contact-form button').text('Verzonden');
+            setTimeout(function () {
+                window.location.replace("http://studiofonkel.nl/bedankt-voor-je-bericht");
+            }, 500);
+        },
+        emails: [
+            {
+                from: 'website@studiofonkel.nl',
+                subject: 'Sollicitatie via studiofonkel.nl',
                 to: ['info@studiofonkel.nl'],
                 fromNameField: 'name',
                 phoneNumber: 'phone',
